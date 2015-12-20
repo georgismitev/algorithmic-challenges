2c12d2698140e423f4ba27e63b21ce1dc38c2a19
georgi <georgi.mitev.dev@gmail.com>
2015-12-20 10:23:12 +0100

    maximizing xor

---
+++ app/maximizing_xor.rb
@@ -0,0 +1,15 @@
+def maxXor(l, r)
+  max = 0
+
+  l.upto(r) do |i|
+    l.upto(r) do |j|
+      max = [i ^ j, max].max
+    end
+  end
+
+  max
+end
+
+l = gets.to_i
+r = gets.to_i
+print maxXor(l, r)
