d2a17075b5a3dbaf59c3a9ce69568a42052e7ea3
georgi <georgi.mitev.dev@gmail.com>
2016-01-03 22:39:10 +0100

    tree rotations

--- app/red_black_tree.rb
+++ app/red_black_tree.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class RedBlackTree
   attr_accessor :root, :size
 
@@ -25,5 +27,70 @@ class RedBlackTree
     end
 
     current_value
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def find_parent(value)
+    node = root
+
+    while !node.nil? do
+      if node.value < value
+        if node.right != RedBlackNilNode.instance
+          node = node.right
+        else
+          break
+        end
+      else
+        if node.left != RedBlackNilNode.instance
+          node = node.left
+        else
+          break
+        end
+      end
+    end
+
+    node
+  end
+
+  def rotate_right(node)
+    parent = node.parent
+    node.parent = parent.parent
+    if parent.parent != RedBlackNilNode.instance
+      if parent.parent.right == parent
+        parent.parent.right = node
+      else
+        parent.parent.left = node
+      end
+    else
+      self.root = node
+    end
+    right = node.right
+    node.right = parent
+    parent.parent = node
+    parent.left = right
+    right.parent = parent if right
+    # change colors
+  end
+
+  def rotate_left(node)
+    parent = node.parent
+    node.parent = parent.parent
+    if parent.parent != RedBlackNilNode.instance
+      if parent.parent.right == parent
+        parent.parent.right = node
+      else
+        parent.parent.left = node
+      end
+    else
+      self.root = node
+    end
+    left = node.left
+    node.left = parent
+    parent.parent = node
+    parent.right = left
+    left.parent = parent if left
   end
 end
--- spec/red_black_tree_spec.rb
+++ spec/red_black_tree_spec.rb
@@ -1,15 +1,130 @@
 require 'spec_helper'
 
 describe RedBlackTree do
-  let(:tree) { RedBlackTree.new }
+  describe '#rotate_right' do
+    before do
+      tree.insert(10)
+      tree.insert(11)
+      tree.insert(8)
+      tree.insert(6)
+      tree.insert(9)
+      expect(tree.size).to eq(5)
+      expect(tree.root.value).to eq(10)
+      expect(tree.root.right.value).to eq(11)
+      expect(tree.root.right.left).to be_a(RedBlackNilNode)
+      expect(tree.root.right.right).to be_a(RedBlackNilNode)
+      expect(tree.root.left.value).to eq(8)
+      expect(tree.root.left.left.value).to eq(6)
+      expect(tree.root.left.left.left).to be_a(RedBlackNilNode)
+      expect(tree.root.left.left.right).to be_a(RedBlackNilNode)
+      expect(tree.root.left.right.value).to eq(9)
+      expect(tree.root.left.right.left).to be_a(RedBlackNilNode)
+      expect(tree.root.left.right.right).to be_a(RedBlackNilNode)
+    end
 
-  describe '#initialize' do
-    it 'creates an empty tree' do
-      expect(tree.size).to eq(0)
-      expect(tree.root).to be_a(RedBlackNilNode)
-      expect(tree.root.color).to eq(:black)
-      expect(tree.root.black?).to be true
-      expect(tree.root.red?).to be false
+    context 'when the parent is root' do
+      it 'updates the root' do
+        tree.rotate_right(tree.root.left)
+        expect(tree.size).to eq(5)
+        expect(tree.root.value).to eq(8)
+        expect(tree.root.left.value).to eq(6)
+        expect(tree.root.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.right.value).to eq(10)
+        expect(tree.root.right.left.value).to eq(9)
+        expect(tree.root.right.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.right.right.value).to eq(11)
+        expect(tree.root.right.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.right.right).to be_a(RedBlackNilNode)
+      end
+    end
+
+    context 'when the parent is not root' do
+      it 'correctly sets the nodes' do
+        tree.insert(5)
+        tree.insert(7)
+        tree.rotate_right(tree.root.left.left)
+        expect(tree.size).to eq(7)
+        expect(tree.root.value).to eq(10)
+        expect(tree.root.right.value).to eq(11)
+        expect(tree.root.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.right).to be_a(RedBlackNilNode)
+        expect(tree.root.left.value).to eq(6)
+        expect(tree.root.left.left.value).to eq(5)
+        expect(tree.root.left.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.value).to eq(8)
+        expect(tree.root.left.right.left.value).to eq(7)
+        expect(tree.root.left.right.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.right.value).to eq(9)
+        expect(tree.root.left.right.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.right.right).to be_a(RedBlackNilNode)
+      end
+    end
+  end
+
+  describe '#rotate_left' do
+    before do
+      tree.insert(10)
+      tree.insert(9)
+      tree.insert(12)
+      tree.insert(11)
+      tree.insert(14)
+      expect(tree.size).to eq(5)
+      expect(tree.root.value).to eq(10)
+      expect(tree.root.left.value).to eq(9)
+      expect(tree.root.left.left).to be_a(RedBlackNilNode)
+      expect(tree.root.left.right).to be_a(RedBlackNilNode)
+      expect(tree.root.right.value).to eq(12)
+      expect(tree.root.right.left.value).to eq(11)
+      expect(tree.root.right.left.left).to be_a(RedBlackNilNode)
+      expect(tree.root.right.left.right).to be_a(RedBlackNilNode)
+      expect(tree.root.right.right.value).to eq(14)
+      expect(tree.root.right.right.left).to be_a(RedBlackNilNode)
+      expect(tree.root.right.right.right).to be_a(RedBlackNilNode)
+    end
+
+    context 'when the parent is root' do
+      it 'updates the root' do
+        tree.rotate_left(tree.root.right)
+        expect(tree.size).to eq(5)
+        expect(tree.root.value).to eq(12)
+        expect(tree.root.right.value).to eq(14)
+        expect(tree.root.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.right).to be_a(RedBlackNilNode)
+        expect(tree.root.left.value).to eq(10)
+        expect(tree.root.left.left.value).to eq(9)
+        expect(tree.root.left.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.value).to eq(11)
+        expect(tree.root.left.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right.right).to be_a(RedBlackNilNode)
+      end
+    end
+
+    context 'when the parent is not root' do
+      it 'correctly sets the nodes' do
+        tree.insert(13)
+        tree.insert(15)
+        tree.rotate_left(tree.root.right.right)
+        expect(tree.root.value).to eq(10)
+        expect(tree.root.left.value).to eq(9)
+        expect(tree.root.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.right.value).to eq(14)
+        expect(tree.root.right.right.value).to eq(15)
+        expect(tree.root.right.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.right.right).to be_a(RedBlackNilNode)
+        expect(tree.root.right.left.value).to eq(12)
+        expect(tree.root.right.left.left.value).to eq(11)
+        expect(tree.root.right.left.left.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.left.left.right).to be_a(RedBlackNilNode)
+        expect(tree.root.right.left.right.value).to eq(13)
+        expect(tree.root.right.left.right.left).to be_a(RedBlackNilNode)
+        expect(tree.root.right.left.right.right).to be_a(RedBlackNilNode)
+      end
     end
   end
 
