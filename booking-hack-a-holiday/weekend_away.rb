257830c1b147f3782bad9c1f1b60af76a91728fb
georgi <georgi.mitev.dev@gmail.com>
2016-02-23 22:46:57 +0100

    speed up the weekend away a little bit

--- booking-hack-a-holiday/weekend_away.rb
+++ booking-hack-a-holiday/weekend_away.rb
@@ -10,7 +10,8 @@ def self.find_min_three_path(graph, start_vertex)
 
   distances.keys.each do |child_vertex|
     graph[child_vertex].keys.each do |v|
-      if v != start_vertex && distances[child_vertex] + graph[child_vertex][v] < min
+      next if v == start_vertex
+      if distances[child_vertex] + graph[child_vertex][v] < min
         min = distances[child_vertex] + graph[child_vertex][v]
       end
     end
@@ -19,21 +20,23 @@ def self.find_min_three_path(graph, start_vertex)
   min
 end
 
-gets.to_i.times do
+t = $stdin.gets.to_i
+k = 0
+while k < t do
   graph = Hash.new { |h, k| h[k] = { } }
 
-  _, m = gets.strip.split.map(&:to_i)
-  m.times do
-    a, b, d = gets.strip.split.map(&:to_i)
+  i = 0
+  _, m = $stdin.gets.split.map!(&:to_i)
+  while i < m do
+    a, b, d = $stdin.gets.split.map!(&:to_i)
     graph[a][b] = d
     graph[b][a] = d
+    i += 1
   end
 
   min_path = MAX
-
-  graph.keys.each do |vertex|
-    min_path = [find_min_three_path(graph, vertex), min_path].min
-  end
-
+  graph.keys.each { |v| min_path = [find_min_three_path(graph, v), min_path].min }
   puts min_path
+
+  k += 1
 end
